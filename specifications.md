2c748088819a3aef9e8a7478eddbb35b7b1544b1
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-04-19 21:46:14 +0545

    working on footer; added md for colour and font specifications

---
+++ specifications.md
@@ -0,0 +1,22 @@
+## Font specifications
+
+Open Sans
+Noto Sans
+Noto Sans Newa
+Alegreya Sans
+Amethysta
+Roboto
+
+## Colour Specifications
+
+### Navbar colours
+
+text-[#D41367]
+bg-[#F5EFE0]
+
+### Homepage colours
+
+- Swiper
+bg-[#f5efe0]
+bg-[#b4ded1]
+bg-[#e0e5f5]
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,29 @@
-import { roboto } from "@/ui/fonts";
+import Link from "next/link";
+import { openSans } from "@/ui/fonts";
 
 export default function Footer() {
     return (
-        <footer className={`${roboto.className} bg-[#cccffe] py-6`}>
-            <div className="max-w-screen-xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-            {/* Footer links and content goes here */}
+        <footer className={`${openSans.className} text-black bg-[#cccffe] py-6`}>
+            {/* Location and contact */}
+            <div className="max-w-screen-xl mx-auto px-4 py-4 flex flex-col justify-between">
+                <div>Queer Youth Group Nepal</div>
+
+                {/* Office physical address link */}
+                <Link
+                    key="location"
+                    href="https://maps.app.goo.gl/6apehbLTBEmZGDva8"
+                >
+                    Yala Dhwakha (Patandhoka), Bagmati, Nepal
+                </Link>
+                 
+                <div>+977 9851350665 </div>
             </div>
             
             {/* Divider line */}
             <div className="mx-auto w-[90%] h-[1px] bg-gray-400"></div>
 
             {/* Bottom text */}
-            <div className="text-center text-xs mt-4 font-open-sans">
+            <div className="text-center text-xs mt-4">
                 &copy; {new Date().getFullYear()} Queer Youth Group. All rights reserved.
             </div>
         </footer>
