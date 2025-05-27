2ea92a1099319f11263d33a6fb7de38d9cd1f969
Sushovan Shakya <sushakya235@proton.me>
2025-05-27 16:46:34 +0545

    have to fix the teams section soon

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="w-full flex flex-col flex-grow bg-[#fafafc] items-center">
+    <div className="w-full flex flex-col flex-grow bg-white items-center">
       <div className="text-center text-2xl">
           <h1 className={`${alegreyaSans.className} text-black font-bold py-4 mt-4`}>Our Team</h1>
       </div>
 
       {/* First row of team */}
       <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 p-4">
-        <div className="bg-white text-black w-[300px] h-[500px] border rounded shadow">
+        <div className="bg-[#F5EFE0] text-black w-[300px] h-[500px] rounded shadow">
           <div className="w-full h-full relative overflow-hidden rounded">
             <Image 
                 key="aboutus"
@@ -20,26 +20,95 @@ export default function Page() {
                 height={300}
                 className="object-cover"
             />
+
+            <div className="font-bold ">
+              Rukshana Kapali
+            </div>
           </div>
         </div>
-        <div className="bg-white text-black w-[300px] h-[500px] p-4 border rounded shadow">
-          Two
+        <div className="bg-[#F5EFE0] text-black w-[300px] h-[500px] rounded shadow">
+          <div className="w-full h-full relative overflow-hidden rounded">
+            <Image 
+                key="aboutus"
+                src="images/nang.jpeg"
+                alt="rukshana"
+                width={300}
+                height={300}
+                className="object-cover"
+            />
+
+            <div className="font-bold ">
+              Nangboong Rai
+            </div>
+          </div>
         </div>
-        <div className="bg-white text-black w-[300px] h-[500px] p-4 border rounded shadow">
-          Three
+        <div className="bg-[#F5EFE0] text-black w-[300px] h-[500px] rounded shadow">
+          <div className="w-full h-full relative overflow-hidden rounded">
+            <Image 
+                key="aboutus"
+                src="images/rita.jpg"
+                alt="rukshana"
+                width={300}
+                height={300}
+                className="object-cover"
+            />
+
+            <div className="font-bold ">
+              Rita
+            </div>
+          </div>
         </div>
       </div>
 
       {/* Second row of team */}
       <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 p-4">
-        <div className="bg-white text-black w-[300px] h-[500px] p-4 border rounded shadow">
-          Four
+        <div className="bg-[#F5EFE0] text-black w-[300px] h-[500px] rounded shadow">
+          <div className="w-full h-full relative overflow-hidden rounded">
+            <Image 
+                key="aboutus"
+                src="images/sudip.jpg"
+                alt="rukshana"
+                width={300}
+                height={300}
+                className="object-cover"
+            />
+
+            <div className="font-bold ">
+              Ankit
+            </div>
+          </div>
         </div>
-        <div className="bg-white text-black w-[300px] h-[500px] p-4 border rounded shadow">
-          Five
+        <div className="bg-[#F5EFE0] text-black w-[300px] h-[500px] rounded shadow">
+          <div className="w-full h-full relative overflow-hidden rounded">
+            <Image 
+                key="aboutus"
+                src="images/chhesing.jpg"
+                alt="rukshana"
+                width={300}
+                height={300}
+                className="object-cover"
+            />
+
+            <div className="font-bold ">
+              Chhesing
+            </div>
+          </div>
         </div>
-        <div className="bg-white text-black w-[300px] h-[500px] p-4 border rounded shadow sm:mb-4">
-          Six
+        <div className="bg-[#F5EFE0] text-black w-[300px] h-[500px] rounded shadow">
+          <div className="w-full h-full relative overflow-hidden rounded">
+            <Image 
+                key="aboutus"
+                src="images/ankita.jpg"
+                alt="rukshana"
+                width={300}
+                height={300}
+                className="object-cover"
+            />
+
+            <div className="font-bold ">
+              Ankita
+            </div>
+          </div>
         </div>
       </div>
     </div>
