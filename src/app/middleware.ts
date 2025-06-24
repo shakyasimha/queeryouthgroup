7fa0daedef4af20b507d3f3af9a8c5c0216d8c36
Sushovan Shakya <sushakya235@proton.me>
2025-06-24 13:56:38 +0545

    changed middleware code

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,10 +1,28 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    // Redirect root to default locale
-    if (request.nextUrl.pathname == "/") {
-        return NextResponse.redirect(new URL('/en', request.url));
+    const pathname = request.nextUrl.pathname; 
+
+    // Skip if alreeady /en or API routes
+    if (pathname.startsWith('/np/') || pathname.startsWith('/api')) {
+        return NextResponse.next();
     }
+
+    // Redirect /np to Nepali
+    if (pathname == '/np') {
+        return NextResponse.redirect(new URL('/np', request.url));
+    }
+
+    /// Serve English content from /en/* but hide /en/ prefix
+    if (!pathname.startsWith('/en/')) {
+        const url = request.nextUrl.clone(); 
+        url.pathname = `/en${pathname}`;
+        return NextResponse.rewrite(url);
+    }
+
+    return NextResponse.next();
 }
 
+export const config = {
+    matcher: ['/'],
+}
--- src/components/Navbar/nav-links.tsx
+++ src/components/Navbar/nav-links.tsx
@@ -1,78 +1,37 @@
-    import Link from "next/link";
-    import clsx from "clsx";
-    import { usePathname } from "next/navigation";
-    import { alegreyaSans } from "@/ui/fonts"
+import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+import { alegreyaSans } from "@/ui/fonts"
 
-<<<<<<< HEAD
-    interface Link {
-        name: string; 
-        href: string;
-    }
-    interface NavLinksProps {
-        className?: string;
-        links: Link[];
-    }
-=======
-// Navbar links are here
-const links = [
-    { 
-        name: 'Home', 
-        href: '/'
-    },
-    {
-        name: 'About Us',
-        href: '/about'
-    },
-    {
-        name: 'Our Team',
-        href: '/team'
-    },
-    {
-        name: 'Research',
-        href: '/research'
-    },
-    {
-        name: 'Legal Resources',
-        href: '/resources'
-    },
-    {
-        name: 'Litigations',
-        href: '/litigations'
-    },
-    {
-        name: 'Publications',
-        href: '/publications'
-    },
-    {
-        name: 'Dictionary',
-        href: '/dictionary'
-    },
-]
-interface NavLinksProps {
-    className?: string;
+interface Link {
+name: string; 
+href: string;
 }
->>>>>>> parent of 992fe74 (added our work section in navbar and content)
+interface NavLinksProps {
+className?: string;
+links: Link[];
+}
 
-    export default function NavLinks({ className, links }: NavLinksProps) {
-        const pathname = usePathname(); // Get current route 
+export default function NavLinks({ className, links }: NavLinksProps) {
+const pathname = usePathname(); // Get current route 
 
-        return (
-            <ul className={`flex space-x-4 ${alegreyaSans.className} ${className}`}>
-                {links.map((link) => (
-                    <li key={link.name}>
-                    <Link
-                        href={link.href}
-                        className={clsx(
-                            "flex items-center px-2 py-2 md:width-auto text-[#d13467] rounded-md transition duration-300 ease-in-out hover:bg-[#d13467] hover:text-white", // default link style
-                                {
-                                    "text-white bg-[#d13467] font-bold transition-all duration-300 ease-in-out": pathname === link.href, /// active link style</li>
-                                }
-                        )}
-                    >
-                        <p className="block">{link.name}</p>
-                    </Link>
-                    </li>
-                ))}
-            </ul>
-        )
-    }
+return (
+    <ul className={`flex space-x-4 ${alegreyaSans.className} ${className}`}>
+        {links.map((link) => (
+            <li key={link.name}>
+            <Link
+                href={link.href}
+                className={clsx(
+                    "flex items-center px-2 py-2 md:width-auto text-[#d13467] rounded-md transition duration-300 ease-in-out hover:bg-[#d13467] hover:text-white", // default link style
+                        {
+                            "text-white bg-[#d13467] font-bold transition-all duration-300 ease-in-out": pathname === link.href, /// active link style</li>
+                        }
+                )}
+            >
+                <p className="block">{link.name}</p>
+            </Link>
+            </li>
+        ))}
+    </ul>
+)
+}
