938e6c27792927264eb6d16a4e54ede66cd54de7
Sushovan Shakya <sushakya235@proton.me>
2025-06-25 12:08:32 +0545

    removed np/ route; will dynamically allocate routes today

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import { redirect } from "next/navigation";
+// import Image from "next/image";
+// import { roboto, openSans, alegreyaSans } from "@/ui/fonts";
+import Slideshow from "@/components/Slideshow";
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
-  redirect('/en');
+  return (
+    <main className="bg-[#FAF9FC] w-full flex flex-col items-center justify-start px-4 pt-4">
+      {/* First container - image and welcome */}
+      <section className="w-full max-w-7xl">
+        <Slideshow />
+      </section>
+
+      {/* You can add a Welcome section, About, etc. below this */}
+      <Welcome />
+    </main>
+  );
 }
