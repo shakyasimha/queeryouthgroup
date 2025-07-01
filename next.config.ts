b018712962f4f8729b58a49a8804a892bb4f1547
Sushovan Shakya <sushakya235@proton.me>
2025-07-01 12:57:50 +0545

    changed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true, // Disable Next.js image optimization; might remove later
-  },
+  // distDir: 'dist',
+  // images: {
+  //   unoptimized: true, // Disable Next.js image optimization; might remove later
+  // },
 };
 
 const withNextIntl = createNextIntlPlugin(); 
