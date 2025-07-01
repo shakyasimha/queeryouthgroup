8ddfedc7cfe909709e5f4e793bf9b0548b4895c3
Sushovan Shakya <sushakya235@proton.me>
2025-07-01 12:20:39 +0545

    removed try-catch block in request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,32 +3,17 @@ import {hasLocale} from 'next-intl';
 import {routing} from './routing';
 
 // type for messages
-// type Message = typeof import('@/messages/en.json');
+type Message = typeof import('@/messages/en.json');
 
 export default getRequestConfig(async ({requestLocale}) => {
-    // Typically corresponds to the `[locale]` segment
-    const requested = await requestLocale;
-    const locale = hasLocale(routing.locales, requested)
+  // Typically corresponds to the `[locale]` segment
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
-    
-    let messages;
-
-    try {
-        // Option 1: Using request (works consistently)
-        messages = (await import(`@/messages/${locale}.json`));
-
-        // Option 2: Using dynamic import (alternative)
-        // messages = (await import(`@/messages/${locale}.json`)).default;
-    } catch(error) {
-        console.error(`Failed to load messages for locale ${locale}`);
-
-        // Fallback to default locale messages
-        messages = (await import(`@/messages/${routing.defaultLocale}.json`));
-    }
-
-    return {
-        locale,
-        messages
-    };
+ 
+  return {
+    locale,
+    messages: (await import(`@/messages/${locale}.json`)).default
+  };
 });
