163cc438fded85c1256ec003f786bea2a8bda3b9
Sushovan Shakya <sushakya235@proton.me>
2025-07-01 09:19:47 +0545

    renamed [lang] to [locale]; added i18n support

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,3 @@
-import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
-
-const locales = ['en', 'ne'];
-
-export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale)) notFound();
-
-  return {
-    messages: (await import(`../locales/${locale}.json`)).default
-  };
-});
+export const locales = ['en', 'ne'] as const;
+export type Locale = typeof locales[number];
+export const defaultLocale: Locale = 'en';
